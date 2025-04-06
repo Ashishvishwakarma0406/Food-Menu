bdad20d61ffa273a48bf7ec37862146756221959
Ashish Vishwakarma <ashishvishwakarma0606@gmail.com>
2025-04-06 21:32:05 +0530

    Add images

--- src/item.js
+++ src/item.js
@@ -1,24 +1,26 @@
+import coffee1 from "./assets/image/coffee1.jpg";
+import coffee2 from "./assets/image/coffee2.jpg";
+
+
 const menuItems = [
-    {
-      id: 1,
-      name: "Veg Biryani",
-      rating: 4,
-      reviews: 9,
-      price: "₹120",
-      description: "A rich blend of chocolate milk and espresso for a deliciously energizing treat",
-      image: "src/assets/image/coffee1.jpg" 
-    },
-    {
-      id: 2,
-      name: "Paneer Butter Masala",
-      rating: 2.3,
-      reviews: 14,
-      price: "₹150",
-      description: "A smooth fusion of chocolate milk and espresso for a deliciously",
-      image: "src/assets/image/coffee2.jpg"
-    },
-    // Add more items...
-  ];
-  
-  export default menuItems;
-  
+  {
+    id: 1,
+    name: "Veg Biryani",
+    rating: 4,
+    reviews: 9,
+    price: "₹120",
+    description: "A rich blend of chocolate milk and espresso...",
+    image: coffee1
+  },
+  {
+    id: 2,
+    name: "Paneer Butter Masala",
+    rating: 2.3,
+    reviews: 14,
+    price: "₹150",
+    description: "A smooth fusion of chocolate milk and espresso...",
+    image: coffee2
+  },
+];
+
+export default menuItems
